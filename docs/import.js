1954874f2ffec0c4ea61ceeb088b34b17acadb1a
jlongyam <jhonliongbox@gmail.com>
2026-02-08 10:54:22 +0700

    fix: mobile Add preview of window.console type before override

--- docs/import.js
+++ docs/import.js
@@ -1,10 +1,13 @@
 import consoleBox from 'https://cdn.jsdelivr.net/gh/jlongyam/consoleBox/dist/es/consoleBox.js';
 
+let preview = document.querySelector('#preview');
+
+preview.innerHTML += typeof window.console + '\n'
 window.defaultConsole = window.console
 delete window.console;
 window.console = consoleBox;
 
-let preview = document.querySelector('#preview');
+
 
 console.dirxml(preview);
 console.dir(preview);
