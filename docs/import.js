ae22cea1bc9356965cfa8c843f2d92deb615cfcb
jlongyam <jhonliongbox@gmail.com>
2026-02-08 10:58:49 +0700

    fix: mobile console not appear

--- docs/import.js
+++ docs/import.js
@@ -3,11 +3,11 @@ import consoleBox from 'https://cdn.jsdelivr.net/gh/jlongyam/consoleBox/dist/es/
 let preview = document.querySelector('#preview');
 
 preview.innerHTML += typeof window.console + '\n'
-window.defaultConsole = window.console
-delete window.console;
-window.console = consoleBox;
+// window.defaultConsole = window.console
+// delete window.console;
+// window.console = consoleBox;
 
 
 
-console.dirxml(preview);
-console.dir(preview);
+// console.dirxml(preview);
+// console.dir(preview);
