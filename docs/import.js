86004ba09b3f103f5bd0c2755c9cba9c8ac9c2d3
jlongyam <jhonliongbox@gmail.com>
2026-02-08 11:35:36 +0700

    feat: enable consoleBox and add debug logs

--- docs/import.js
+++ docs/import.js
@@ -3,13 +3,11 @@ import { consoleBox } from 'https://cdn.jsdelivr.net/gh/jlongyam/consoleBox@main
 
 let preview = document.querySelector('#preview');
 
-preview.innerHTML += typeof window.console + '\n'
-preview.innerHTML += typeof consoleBox + '\n'
-// window.defaultConsole = window.console
-// delete window.console;
-// window.console = consoleBox;
+window.defaultConsole = window.console
+delete window.console;
+window.console = consoleBox;
 
 
 
-// console.dirxml(preview);
-// console.dir(preview);
+console.dirxml(preview);
+console.dir(preview);
